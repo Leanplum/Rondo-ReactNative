8ae6a89cd5b76182abfbe5a180819782ddf3a46c
AleksandarAndov <AleksandarAndov@172.16.10.160>
2020-01-27 17:53:13 +0200

    feat: define variables POC

--- android/app/src/main/java/com/rondoapp/LeanplumSdkModule.java
+++ android/app/src/main/java/com/rondoapp/LeanplumSdkModule.java
@@ -5,18 +5,34 @@ import android.location.Location;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 
 
 import com.facebook.react.bridge.ReadableMap;
 import com.leanplum.Leanplum;
 import com.leanplum.LeanplumLocationAccuracyType;
+import com.leanplum.annotations.Parser;
 
 import static com.leanplum.Leanplum.getContext;
+import com.leanplum.Var;
+import android.util.Log;
+import com.facebook.react.common.ReactConstants;
+import com.leanplum.callbacks.VariableCallback;
+import com.leanplum.callbacks.StartCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class LeanplumSdkModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private static final String TAG = LeanplumSdkModule.class.getName();
+    public static Map<String, String> variables = new HashMap<String, String>();
+    public static List<Var<String>> variablesList = new ArrayList<Var<String>>();
+
 
 
     public LeanplumSdkModule(ReactApplicationContext reactContext) {
@@ -53,10 +69,67 @@ public class LeanplumSdkModule extends ReactContextBaseJavaModule {
     public void setUserAttributes(ReadableMap attributes) {
         Leanplum.setUserAttributes(attributes.toHashMap());
     }
-
+    
+    @ReactMethod
+    public void setVariables(ReadableMap attributes) {
+        Log.d(ReactConstants.TAG, "SET VARIABLES");
+        Log.d(ReactConstants.TAG, "attributes:" + attributes.toString());
+        ReadableMapKeySetIterator iterator = attributes.keySetIterator();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            Log.d(ReactConstants.TAG, "KEY:" + key);
+            Log.d(ReactConstants.TAG, "VAL:" + attributes.getString(key));
+            variablesList.add(Var.define(key, attributes.getString(key)));
+        }
+        //Log.d(ReactConstants.TAG, attributes.toString());
+        //Log.d(ReactConstants.TAG, attributes.toHashMap().toString());
+        //LeanplumSdkModule.variables = attributes.toHashMap();
+        //return LeanplumSdkModule.variables.get("welcomeLabel");
+    }
+    
     @ReactMethod
     public void start() {
+        if (BuildConfig.DEBUG) {
+            Log.d(ReactConstants.TAG, "DEV MODE");
+        }
+        //Var<String> welcomeLabel = Var.define("welcomeLabel", "Welcome!");
+        //Log.d(ReactConstants.TAG, welcomeLabel.toString());
+        Parser.parseVariables(LeanplumSdkModule.variablesList);
+        Log.d(ReactConstants.TAG, "PARSE FINISHED");
         Leanplum.start(getContext());
+        Log.d(ReactConstants.TAG, "START FINISHED");
+        //Leanplum.forceContentUpdate();
+        //Log.d(ReactConstants.TAG, "FORCE UPDATE FINISHED");
+        // Then add a handler and pass it a new callback.
+        Log.d(ReactConstants.TAG, "variablesList: " + String.valueOf(variablesList.size()));
+        for (Var<String> varaibleDefinied : variablesList){
+            Log.d(ReactConstants.TAG, "variablesList step");
+            varaibleDefinied.addValueChangedHandler(new VariableCallback<String>() {
+                @Override
+                public void handle(Var<String> var) {
+                    //TODO invoke RN function
+                    Log.d(ReactConstants.TAG, "VALUE CHANGED TRIGGERED");
+                    LeanplumSdkModule.variables.put(var.name(), var.stringValue());
+                    Log.d(ReactConstants.TAG, var.name());
+                    Log.d(ReactConstants.TAG, var.stringValue());
+                }
+            });
+        }
+        /////////////////////////////////////////////////////////
+        // Leanplum.addStartResponseHandler(new StartCallback() {
+        //     @Override
+        //     public void onResponse(boolean b) {
+        //         Log.d(ReactConstants.TAG, "CALLBACK TRIGGERED");
+        //         Log.d(ReactConstants.TAG, Boolean.toString(b));
+        //         // Insert code here.
+        //     }
+        // });
+    }
+
+
+    @ReactMethod
+    public void forceContentUpdate() {
+        Leanplum.forceContentUpdate();
     }
 
     @ReactMethod
@@ -73,6 +146,12 @@ public class LeanplumSdkModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void disableLocationCollection() {
         Leanplum.disableLocationCollection();
+    }
+    
+    @ReactMethod
+    public void parseVariables() {
+        Var<String> welcomeLabel = Var.define("welcomeLabel", "Welcome!");
+        Parser.parseVariables(welcomeLabel);
     }
 
     @ReactMethod
