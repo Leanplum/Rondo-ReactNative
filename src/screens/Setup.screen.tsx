f475ebf46ac153c56f0909585c3b7616b1720e31
Alik <alik.risco@musala.com>
2020-01-27 18:19:03 +0200

    feat: android permission

--- src/screens/Setup.screen.tsx
+++ src/screens/Setup.screen.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ScrollView, SafeAreaView, StyleSheet} from 'react-native';
 import {CreateApp, Device} from 'components';
+import {requestCameraPermission} from 'utils';
 
 export const SetupScreen = () => {
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
---
+++ src/utils/location.permission.ts
@@ -0,0 +1,16 @@
+import {PermissionsAndroid} from 'react-native';
+
+export const requestCameraPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the location');
+    } else {
+      console.log('Location permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
