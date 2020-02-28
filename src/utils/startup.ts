3fa6b6e7bd48ef13e86bcb5620c76590fbf47898
Alik <alik.risco@musala.com>
2020-02-28 17:00:30 +0200

    feat: set Rn Rondo as default app

--- src/utils/startup.ts
+++ src/utils/startup.ts
@@ -1,8 +1,9 @@
 import {Leanplum} from 'react-native-leanplum';
 import {requestLocationPermission} from './location.permission';
 import {ASSET_VARIABLE_NAME} from 'utils';
+import {AppsStorage} from './apps.storage';
 
-export const startUp = ({
+export const startUp = async ({
   variables,
   setVariables,
   path,
@@ -17,6 +18,16 @@ export const startUp = ({
     console.log({success});
   });
   Leanplum.setVariables(variables);
+
+  const apps = await AppsStorage.getAll();
+  if (!apps.length) {
+    await AppsStorage.save({
+      appId: 'app_mdPnGAyQhzV5CcibMb9d9GDQ7oj1J94odFm6lunFd2I',
+      productionKey: 'prod_rNf462v60Cl3KA9ntyCiQQup03VyZmkV1Ly21tgKfzg',
+      developmentKey: 'dev_S73p5EOeSmH5U2fmT5sH0DENA16qWSnWisUIJtO33qM',
+      name: 'RN Rondo',
+    });
+  }
   // Leanplum.onVariablesChangedAndNoDownloadsPending(() => {
   //   console.log('onVariablesChangedAndNoDownloadsPending');
   // });
