43da0519089fce7e43aef68e67380ca4d5475b5a
Alik <alik.risco@musala.com>
2020-02-28 18:31:53 +0200

    feat: app settings

--- src/components/setup/session.component.tsx
+++ src/components/setup/session.component.tsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
-import {Text} from 'react-native-elements';
+import {Text, Divider} from 'react-native-elements';
 import {Leanplum} from 'react-native-leanplum';
 import {withNavigation, NavigationScreenProp} from 'react-navigation';
+import {AppsStorage, LeanplumAppConfig} from 'utils';
 
 const SessionComponent = ({
   navigation,
@@ -12,12 +13,16 @@ const SessionComponent = ({
   const [deviceId, setDeviceId] = useState('');
   const [userId, setUserId] = useState('');
 
+  const [app, setApp] = useState<LeanplumAppConfig>();
+
   useEffect(() => {
     async function getSession() {
       const userIdValue = await Leanplum.userId();
       const deviceIdValue = await Leanplum.deviceId();
       setUserId(userIdValue);
       setDeviceId(deviceIdValue);
+      const app = await AppsStorage.currentApp();
+      setApp(app);
     }
     navigation.addListener('didFocus', () => {
       getSession();
@@ -26,13 +31,35 @@ const SessionComponent = ({
 
   return (
     <View style={styles.container}>
-      <View style={styles.propertyView}>
-        <Text style={styles.label}>UserId: </Text>
-        <Text style={styles.value}>{userId}</Text>
+      <View>
+        <Text>Application Settings</Text>
+        <View style={styles.propertyView}>
+          <Text style={styles.label}>App Name: </Text>
+          <Text style={styles.value}>{app?.name}</Text>
+        </View>
+        <View style={styles.propertyView}>
+          <Text style={styles.label}>App Id: </Text>
+          <Text style={styles.appValue}>{app?.appId}</Text>
+        </View>
+        <View style={styles.propertyView}>
+          <Text style={styles.label}>Dev Key: </Text>
+          <Text style={styles.appValue}>{app?.developmentKey}</Text>
+        </View>
+        <View style={styles.propertyView}>
+          <Text style={styles.label}>Prod Key: </Text>
+          <Text style={styles.appValue}>{app?.productionKey}</Text>
+        </View>
       </View>
-      <View style={styles.propertyView}>
-        <Text style={styles.label}>DeviceId: </Text>
-        <Text style={styles.value}>{deviceId}</Text>
+      <View>
+        <Text>Application Settings</Text>
+        <View style={styles.propertyView}>
+          <Text style={styles.label}>UserId: </Text>
+          <Text style={styles.value}>{userId}</Text>
+        </View>
+        <View style={styles.propertyView}>
+          <Text style={styles.label}>DeviceId: </Text>
+          <Text style={styles.value}>{deviceId}</Text>
+        </View>
       </View>
     </View>
   );
@@ -52,6 +79,10 @@ const styles = StyleSheet.create({
   },
   value: {
     fontSize: 14,
+    color: 'gray',
+  },
+  appValue: {
+    fontSize: 11,
     color: 'gray',
   },
   button: {
