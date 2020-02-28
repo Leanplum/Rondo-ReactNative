ea4d690aa1c50faa6ea31cf6100da2cf7c8a6a58
Alik <alik.risco@musala.com>
2020-02-28 17:49:49 +0200

    feat: use previous app

--- src/utils/apps.storage.ts
+++ src/utils/apps.storage.ts
@@ -9,6 +9,7 @@ export interface LeanplumAppConfig {
 
 export class AppsStorage {
   private static readonly appsKey = 'apps';
+  private static readonly currentAppKey = 'currentAppId';
 
   static async save(app: LeanplumAppConfig): Promise<void> {
     const apps = await this.getAll();
@@ -19,5 +20,16 @@ export class AppsStorage {
   static async getAll(): Promise<LeanplumAppConfig[]> {
     const apps = await AsyncStorage.getItem(this.appsKey);
     return JSON.parse(apps || '[]');
+  }
+
+  static async currentApp(): Promise<LeanplumAppConfig | undefined> {
+    try {
+      const appId = await AsyncStorage.getItem(this.currentAppKey);
+      const apps = await this.getAll();
+      const app = apps.find((app: LeanplumAppConfig) => app.appId === appId);
+      return app;
+    } catch (e) {
+      return undefined;
+    }
   }
 }
--- src/utils/startup.ts
+++ src/utils/startup.ts
@@ -1,7 +1,7 @@
 import {Leanplum} from 'react-native-leanplum';
 import {requestLocationPermission} from './location.permission';
 import {ASSET_VARIABLE_NAME} from 'utils';
-import {AppsStorage} from './apps.storage';
+import {AppsStorage, LeanplumAppConfig} from './apps.storage';
 
 export const startUp = async ({
   variables,
@@ -13,39 +13,54 @@ export const startUp = async ({
   setVariables: any;
 }) => {
   requestLocationPermission();
+  registerVariablesAndCallbacks(variables, setVariables, path, setPath);
+  await selectAppAndStart();
+};
 
+const selectAppAndStart = async () => {
+  const defaultApp: LeanplumAppConfig = {
+    appId: 'app_mdPnGAyQhzV5CcibMb9d9GDQ7oj1J94odFm6lunFd2I',
+    productionKey: 'prod_rNf462v60Cl3KA9ntyCiQQup03VyZmkV1Ly21tgKfzg',
+    developmentKey: 'dev_S73p5EOeSmH5U2fmT5sH0DENA16qWSnWisUIJtO33qM',
+    name: 'RN Rondo',
+  };
+
+  const apps = await AppsStorage.getAll();
+  if (!apps.length) {
+    await AppsStorage.save(defaultApp);
+  }
+
+  let currentApp = (await AppsStorage.currentApp()) || defaultApp;
+  Leanplum.setAppIdForDevelopmentMode(
+    currentApp?.appId,
+    currentApp?.developmentKey,
+  );
+  Leanplum.start();
+};
+
+const registerVariablesAndCallbacks = (
+  variables: any,
+  setVariables: any,
+  path: string,
+  setPath: any,
+) => {
   Leanplum.onStartResponse((success: boolean) => {
     console.log({success});
   });
   Leanplum.setVariables(variables);
-
-  const apps = await AppsStorage.getAll();
-  if (!apps.length) {
-    await AppsStorage.save({
-      appId: 'app_mdPnGAyQhzV5CcibMb9d9GDQ7oj1J94odFm6lunFd2I',
-      productionKey: 'prod_rNf462v60Cl3KA9ntyCiQQup03VyZmkV1Ly21tgKfzg',
-      developmentKey: 'dev_S73p5EOeSmH5U2fmT5sH0DENA16qWSnWisUIJtO33qM',
-      name: 'RN Rondo',
-    });
-  }
-  // Leanplum.onVariablesChangedAndNoDownloadsPending(() => {
-  //   console.log('onVariablesChangedAndNoDownloadsPending');
-  // });
-  // Leanplum.onceVariablesChangedAndNoDownloadsPending(() => {
-  //   console.log('onceVariablesChangedAndNoDownloadsPending');
-  // });
-  // Leanplum.onMessageDisplayed((message: any) => {
-  //   console.log({message});
-  // });
-  // Leanplum.onVariablesChanged((value: any) => {
-  //   setVariables(value);
-  // });
-  // Leanplum.setVariableAsset(ASSET_VARIABLE_NAME, path, (newPath: string) =>
-  //   setPath(newPath),
-  // );
-  // Leanplum.setAppIdForDevelopmentMode(
-  //   'app_mdPnGAyQhzV5CcibMb9d9GDQ7oj1J94odFm6lunFd2I',
-  //   'dev_S73p5EOeSmH5U2fmT5sH0DENA16qWSnWisUIJtO33qM',
-  // );
-  // Leanplum.start();
+  Leanplum.onVariablesChangedAndNoDownloadsPending(() => {
+    console.log('onVariablesChangedAndNoDownloadsPending');
+  });
+  Leanplum.onceVariablesChangedAndNoDownloadsPending(() => {
+    console.log('onceVariablesChangedAndNoDownloadsPending');
+  });
+  Leanplum.onMessageDisplayed((message: any) => {
+    console.log({message});
+  });
+  Leanplum.onVariablesChanged((value: any) => {
+    setVariables(value);
+  });
+  Leanplum.setVariableAsset(ASSET_VARIABLE_NAME, path, (newPath: string) =>
+    setPath(newPath),
+  );
 };
