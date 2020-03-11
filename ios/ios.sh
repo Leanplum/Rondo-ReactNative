94ad336225ed29f558229e978540e18f575afe94
AleksandarAndov <AleksandarAndov@10.1.3.19>
2020-03-11 17:26:05 +0200

    add apple cert and fix ios script

--- ios/ios.sh
+++ ios/ios.sh
@@ -5,7 +5,7 @@ security unlock-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # IMPORT ASSETS INTO KEY CHAIN
 security import AppleWWDRCA.cer -k ios-build.keychain -A
-security import development-cert.p12 -k ios-build.keychain -P $SECURITY_PASSWORD -A
+security import 'iPhone distribution.p12' -k ios-build.keychain -P $SECURITY_PASSWORD -A
 security set-key-partition-list -S apple-tool:,apple: -s -k $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain > /dev/null
 # PROVISIONING PROFILE
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
