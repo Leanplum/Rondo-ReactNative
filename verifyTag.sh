e4de04ecee05c75e6ab3a251e3386d10f8d07b34
AleksandarAndov <AleksandarAndov@DESKTOP-BC2IH3E>
2020-03-23 11:01:10 +0200

    change set deploy_app variable

--- verifyTag.sh
+++ verifyTag.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ -n "${LEANPLUM_SDK_VERSION}" ]]; then
     if git tag $LEANPLUM_SDK_VERSION; then 
-        export DEPLOY_APP="true"
+        DEPLOY_APP="true"
     else 
-        export DEPLOY_APP="false"
+        DEPLOY_APP="false"
     fi
 else
-    export DEPLOY_APP="false"
+    DEPLOY_APP="false"
 fi
