48e8ab439c144e3f03858f7ffae15b3bce09859a
AleksandarAndov <AleksandarAndov@DESKTOP-BC2IH3E>
2020-03-23 11:12:51 +0200

    test set env variables

--- verifyTag.sh
+++ verifyTag.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ -n "${LEANPLUM_SDK_VERSION}" ]]; then
     if git tag $LEANPLUM_SDK_VERSION; then 
-        DEPLOY_APP="true"
+        $DEPLOY_APP="true1"
     else 
-        DEPLOY_APP="false"
+        $DEPLOY_APP="false1"
     fi
 else
-    DEPLOY_APP="false"
+    $DEPLOY_APP="false1"
 fi
