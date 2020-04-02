6fe8fb13b27079d25193151fafd0e820cba96dd4
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-04-01 22:02:58 -0700

    feat: add rich push extension

---
+++ ios/Rich Push/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  Rich Push
+//
+//  Created by Mayank Sanganeria on 4/1/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
